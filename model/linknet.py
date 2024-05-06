e2881ce6acedabe29e3754f870425fdc76055a88
Ahmed Maher <ahmedelseaidy22@gmail.com>
2024-05-06 22:02:17 +0300

    updating server services

---
+++ model/linknet.py
@@ -0,0 +1,33 @@
+import os
+import numpy as np
+from utilities.utility import get_model_weights
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['SM_FRAMEWORK'] = 'tf.keras'
+import segmentation_models as sm
+
+
+class LinknetSegmentationModel:
+    def __init__(self, backbone='efficientnetb5', weights_path=get_model_weights('linknet')):
+        self.BACKBONE = backbone
+        self.preprocess_input = sm.get_preprocessing(self.BACKBONE)
+        self.model = sm.Linknet(self.BACKBONE, encoder_weights='imagenet')
+        self.model.compile('AdamW', loss=sm.losses.bce_jaccard_loss, metrics=[sm.metrics.iou_score])
+        self.model.load_weights(weights_path)
+
+    def preprocess_image(self, image):
+        preprocessed_image = self.preprocess_input(image)
+        return preprocessed_image
+
+    def predict(self, image, threshold=None):
+        # Preprocess the input image
+        preprocessed_image = self.preprocess_image(image)
+
+        # Perform prediction using the loaded model
+        predictions = self.model.predict(np.expand_dims(preprocessed_image, axis=0))
+
+        # Process predictions as needed
+        if threshold:
+            predictions = (predictions > threshold).astype(np.uint8)
+
+        return predictions
--- routes/prediction_routes.py
+++ routes/prediction_routes.py
@@ -1,7 +1,7 @@
 from utilities.metric import iou_score, dice_score
+from model.linknet import LinknetSegmentationModel
 from utilities.image_handler import save_images
 from model.unet import UnetSegmentationModel
-import segmentation_models as sm
 from flask import Blueprint
 from flask import request
 import numpy as np
@@ -73,3 +73,64 @@ def predict_unet():
         return response, 201
     else:
         return 'Invalid file format. Please upload images in (TIF, JPEG, JPG, PNG, or GIF) format', 400
+
+
+@prediction_route.route('/predict/linknet', methods=['POST'])
+def predict_linknet():
+    # Check if both "image" and "mask" files were uploaded
+    if 'image' not in request.files or 'mask' not in request.files:
+        return 'Both image and mask files are required', 400
+
+    # Get image and mask files from request
+    image_file = request.files['image']
+    mask_file = request.files['mask']
+
+    # If the user does not select a file, the browser submits an empty file without a filename
+    if image_file.filename == '' or mask_file.filename == '':
+        return 'Both image and mask files must be selected', 400
+
+    # If the files are provided, and they have the allowed file extensions
+    if image_file.filename.endswith(('.jpg', '.jpeg', '.png', '.gif', '.tif')) and \
+            mask_file.filename.endswith(('.jpg', '.jpeg', '.png', '.gif', '.tif')):
+        # Declaring static path
+        static_path = "static/images/{}".format(uuid.uuid4())
+
+        # Read the image file
+        image_bytes = image_file.read()
+        np_image = np.frombuffer(image_bytes, np.uint8)
+        image = cv2.imdecode(np_image, cv2.IMREAD_COLOR)
+
+        # Read the mask file
+        mask_bytes = mask_file.read()
+        np_mask = np.frombuffer(mask_bytes, np.uint8)
+        mask = cv2.imdecode(np_mask, cv2.IMREAD_GRAYSCALE)
+
+        if image.shape != (512, 512, 3) or mask.shape != (512, 512):
+            return 'Image shape must be (512, 512, 3) and mask shape must be (512, 512)', 400
+
+        # Add extra dimension to mask if it's not (512, 512, 1)
+        if mask.ndim == 2:
+            mask = np.expand_dims(mask, axis=-1)
+
+        # time to make a prediction
+        threshold = 0.5
+        unet = LinknetSegmentationModel()
+        prediction = unet.predict(image, threshold=threshold)[0]
+
+        # Make the mask binary for the next stages
+        binary_mask = np.where(mask == 255, 1, 0)
+        binary_mask = binary_mask.astype(np.uint8)
+
+        # Set the response dictionary
+        response = save_images(static_path, image, binary_mask, prediction, threshold=threshold)
+        response.update({
+            "iou_score": iou_score(y_true=binary_mask, y_hat=prediction),
+            "dice_score": dice_score(y_true=binary_mask, y_hat=prediction),
+            "threshold": threshold*100
+        })
+
+        return response, 201
+    else:
+        return 'Invalid file format. Please upload images in (TIF, JPEG, JPG, PNG, or GIF) format', 400
+
+
